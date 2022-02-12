afaf49a75db44f4786ea4ad97f2715f6d8cee235
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2022-02-12 18:42:09 +0300

    start java camp project.

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
@@ -20,10 +24,19 @@ public class ProductManager implements ProductService{
 		super();
 		this.productDao = productDao;
 	}
+	
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		
-		return this.productDao.findAll();
+		return new SuccessDataResult(this.productDao.findAll(),"Data listelendi.");
+	}
+	
+	
+	@Override
+	public Result add(Product product) {
+		
+		this.productDao.save(product);
+		return new SuccessResult("Urun eklendi.");
 	}
 
 }
